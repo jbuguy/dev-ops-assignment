e18775f1ffb99f3f7773f332aea0371799da6ace
jbuguy <wassimnefzi120@gmail.com>
2025-11-16 21:09:23 +0100

    fixed linting warning

--- tests/test_iris.py
+++ tests/test_iris.py
@@ -22,4 +22,6 @@ def test_model_prediction():
     model.train([[5.1, 3.5, 1.4, 0.2], [6.7, 3.0, 5.2, 2.3]], [0, 2])
     predictions = model.predict([[5.0, 3.6, 1.4, 0.2], [6.5, 3.0, 5.2, 2.0]])
     assert len(predictions) == 2
-    assert all(isinstance(pred, (np.int32, np.int64, int)) for pred in predictions)
+    assert all(
+        isinstance(pred, (np.int32, np.int64, int)) for pred in predictions
+        )
